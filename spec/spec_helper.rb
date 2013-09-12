5d8b939b993f37b29c0048159620ba0fdc3055f1
kuying <kuying@kuyingtekiiMac.local>
2013-09-12 20:25:31 +0800

    testcase

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'email_spec'
 require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -48,6 +49,14 @@ RSpec.configure do |config|
   end
 
   config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
     DatabaseCleaner.clean
   end
+  
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
+  
 end
